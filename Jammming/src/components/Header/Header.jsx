64dba6328ef1c4b2adc536342404487dcaf72a17
Colton B <coltonbaldwin101@gmail.com>
2026-02-03 11:42:31 -0500

    Finilizing styling for the header portion including font changes, background-img changes, as well as changing the background style of the overall website (found in .appcontainer of App.cs) to better fit the stylization of the header

--- Jammming/src/components/Header/Header.jsx
+++ Jammming/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <div className={styles.headerContainer}>
         <div className={styles.textContainer}>
-            <h1>Jamming Spotify Playlist Maker</h1>
-            <p>Create spotify playlist for your own personal account here!</p>
+            <h1 className={styles.h1}>Jamming Spotify Playlist Maker</h1>
+            <p className={styles.p}>Search for songs and create your own playlist that you can save on your own Spotify account!</p>
         </div>
     </div>
   );
